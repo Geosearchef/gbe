1d00e94f788d3f6f9f5403a4dd9d2cb233d4f395
Geosearchef <geosearchef@googlemail.com>
2023-07-03 23:48:10 +0200

    rebuild send/receiver panel to use JPanel instead of rendered list

--- src/main/kotlin/ui/honey/SendPanel.kt
+++ src/main/kotlin/ui/honey/SendPanel.kt
@@ -7,7 +7,7 @@ import javax.swing.BorderFactory.createEmptyBorder as emptyBorder
 
 class SendPanel : JPanel() {
 
-    val receiverList = JList<BroadcastProtocolMessage.PeerInfo>()
+    val receiverList = JPanel()
 
     init {
         this.layout = BorderLayout()
@@ -16,75 +16,95 @@ class SendPanel : JPanel() {
         this.maximumSize = Dimension(Int.MAX_VALUE, 300)
         this.preferredSize = Dimension(400, 300)
 
-//        this.border = BorderFactory.createCompoundBorder(
-//            BorderFactory.createLineBorder(Color.red),
-//            this.border
-//        )
-
         this.add(JLabel("Send").apply {
             font = font.deriveFont(Font.BOLD, 16.0f)
             border = emptyBorder(0, 0, 5, 0)
         }, BorderLayout.NORTH)
 
-        receiverList.cellRenderer = ReceiverListCellRenderer
-        receiverList.visibleRowCount = 6
-        this.add(receiverList, BorderLayout.CENTER)
-
-        // TODO
-        receiverList.addListSelectionListener {
-            val fileChooser = JFileChooser()
-            fileChooser.showOpenDialog(this)
-            receiverList.selectedIndex = -1
-        }
+        receiverList.layout = BoxLayout(receiverList, SwingConstants.VERTICAL)
+        this.add(receiverList)
     }
 
     fun setReceiverList(receivers: List<BroadcastProtocolMessage.PeerInfo>) {
-        receiverList.setListData(receivers.toTypedArray())
+        receiverList.removeAll()
+        receivers.forEach { receiverList.add(receiverListCellElementFactory(it)) }
+
+        this.revalidate()
+        this.repaint()
     }
 
+    fun onSendFileClicked(receiver: BroadcastProtocolMessage.PeerInfo) {
+        val fileChooser = JFileChooser()
+        val result = fileChooser.showOpenDialog(this)
 
-    object ReceiverListCellRenderer : ListCellRenderer<BroadcastProtocolMessage.PeerInfo> {
-        override fun getListCellRendererComponent(
-            list: JList<out BroadcastProtocolMessage.PeerInfo>?,
-            value: BroadcastProtocolMessage.PeerInfo?,
-            index: Int,
-            isSelected: Boolean,
-            cellHasFocus: Boolean
-        ): Component {
-            var backgroundColor = list?.background ?: Color.WHITE
-            if(isSelected) {
-                backgroundColor = Color(220, 220, 220)
-            }
-
-            val infoPanel = JPanel()
-            infoPanel.background = backgroundColor
-
-            infoPanel.layout = GridLayout(2, 2, 0, 0)
-
-            infoPanel.add(JLabel(value?.identifier ?: "???").apply { font = font.deriveFont(Font.BOLD, 14.0f) })
-            infoPanel.add(JLabel("ver: ${value?.version ?: "???"}").apply {
-                font = font.deriveFont(10.0f)
-                horizontalAlignment = SwingConstants.RIGHT
-            })
-            infoPanel.add(JLabel(value?.system?.replace("_", " - ") ?: "???").apply { font = font.deriveFont(10.0f) })
-
-            val cellPanel = JPanel()
-            cellPanel.layout = BorderLayout()
-            cellPanel.background = backgroundColor
-            cellPanel.border = emptyBorder(10, 10, 10, 10)
-
-            cellPanel.add(infoPanel, BorderLayout.WEST)
-
-            val sendButton = JButton("Send").apply {
-                horizontalAlignment = SwingConstants.CENTER
-                verticalAlignment = SwingConstants.CENTER
-            }
-            sendButton.addActionListener { println("I was clicked") }
-            cellPanel.add(sendButton, BorderLayout.EAST)
-
-
-            return cellPanel
+        if(result == JFileChooser.APPROVE_OPTION) {
+            val path = fileChooser.selectedFile.toPath() // TODO: multiple files
+            println("Selected file ${path.toAbsolutePath()} to send to ${receiver.identifier}")
         }
-
     }
+
+    fun onSendTextClicked(receiver: BroadcastProtocolMessage.PeerInfo) {
+        val text = JOptionPane.showInputDialog(this, "Text to send:", "Send Text", JOptionPane.QUESTION_MESSAGE)
+        println("Entered text \"$text\" to send to ${receiver.identifier}")
+    }
+
+    fun receiverListCellElementFactory(receiver: BroadcastProtocolMessage.PeerInfo) : JPanel {
+        val backgroundColor = Color.WHITE
+
+        val infoPanel = JPanel()
+        infoPanel.background = backgroundColor
+
+        infoPanel.layout = GridLayout(2, 2, 0, 0)
+
+        infoPanel.add(JLabel(receiver.identifier).apply { font = font.deriveFont(Font.BOLD, 14.0f) })
+        infoPanel.add(JLabel("ver: ${receiver.version}").apply {
+            font = font.deriveFont(10.0f)
+            horizontalAlignment = SwingConstants.RIGHT
+        })
+        infoPanel.add(JLabel(receiver.system.replace("_", " - ")).apply { font = font.deriveFont(10.0f) })
+
+        val cellPanel = JPanel()
+        cellPanel.layout = BorderLayout()
+        cellPanel.background = backgroundColor
+        cellPanel.border = emptyBorder(10, 10, 10, 10)
+        cellPanel.maximumSize = Dimension(Int.MAX_VALUE, 80)
+
+        cellPanel.add(infoPanel, BorderLayout.WEST)
+
+        val buttonPanel = JPanel()
+        buttonPanel.layout = BoxLayout(buttonPanel, SwingConstants.VERTICAL)
+        buttonPanel.background = backgroundColor
+
+        val sendFileButton = JButton("Send File").apply {
+            horizontalAlignment = SwingConstants.CENTER
+            verticalAlignment = SwingConstants.CENTER
+        }
+        Dimension(100, 30).let {
+            sendFileButton.maximumSize = it
+            sendFileButton.minimumSize = it
+        }
+        sendFileButton.setMinMaxSize(Dimension(100, 35))
+        sendFileButton.alignmentY = Component.CENTER_ALIGNMENT
+        sendFileButton.addActionListener { onSendFileClicked(receiver) }
+        buttonPanel.add(sendFileButton)
+
+        val sendTextButton = JButton("Send Text").apply {
+            horizontalAlignment = SwingConstants.CENTER
+            verticalAlignment = SwingConstants.CENTER
+        }
+        sendTextButton.setMinMaxSize(Dimension(100, 25))
+        sendTextButton.alignmentY = Component.CENTER_ALIGNMENT
+        sendTextButton.addActionListener { onSendTextClicked(receiver) }
+        buttonPanel.add(sendTextButton)
+
+        cellPanel.add(buttonPanel, BorderLayout.EAST)
+
+        return cellPanel
+    }
+
+    fun JComponent.setMinMaxSize(dim: Dimension) {
+        this.minimumSize = dim
+        this.maximumSize = dim
+    }
+
 }
